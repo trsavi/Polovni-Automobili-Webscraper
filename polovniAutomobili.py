cd3bf9f377bc88677d802ed7d5e85e3b305751b1
Vukasin <vukasin.vasiljevic@gmail.com>
2019-07-11 00:46:28 +0200

    modified and added new functions

--- polovniAutomobili.py
+++ polovniAutomobili.py
@@ -4,13 +4,13 @@ import requests
 import urllib.request
 from bs4 import BeautifulSoup as bs
 import mysql.connector
-
+"""
 mydb = mysql.connector.connect(
 	host='localhost',
 	user='root',
 	passwd=''
 )
-
+"""
 #print(mydb)
 
 def parse_page(url):
@@ -48,13 +48,58 @@ def all_Brands():
 # get all cars from one brand and model
 def get_cars(brand, model):
 
-	#model[]=
+	brand = (brand.replace(' ', '-')).lower()
+	model = (model.replace(' ', '-')).lower()
+	url ='https://www.polovniautomobili.com/auto-oglasi/pretraga?brand=' + brand + '&model[]=' + model
+
+	#soup = parse_page(url)
+	"""
+	for i in range(1,20):
+		soup = parse_page(url+'&page='+i)
+		pages = soup.findAll('article')
+
+		for page in pages:
+			try:
+				link = page.find('h2')
+
+	"""
+	soup = parse_page(url)
+	pages = soup.findAll('article')
+	#title = soup.findAll(class_="ga-title")
+	"""
+	for t in title:
+		t = t.get('title')
+		print(t)
+	"""
+	for page in pages:
+		#print(page)
+		print("")
+		pageT = page.find('a')
+		#print(pageT)
+
+		title = pageT.get('title')
+		print(title)
+		
+		discount = page.find(class_='price price-discount')
+		#print(discount.get_text())
+		if discount!=None:
+			pass
+			print(discount.get_text())
+		else:
+			price = page.find(class_='price')
+			print(price.get_text())
+		content = page.findAll(class_='inline-block')
+		for con in content:
+			print(con.get_text())
+
+	
+	#print(content)
 
 
+get_cars('Alfa Romeo','75')
 
 
-
-brands = all_Brands()
+#brands = all_Brands()
 """
 for brand in brands:
 	print(brand)
