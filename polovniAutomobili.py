a573869b935264f379548e1bef921585b81f4245
Vukasin <vukasin.vasiljevic@gmail.com>
2019-07-12 18:43:01 +0200

    inserted data into database successfully

--- polovniAutomobili.py
+++ polovniAutomobili.py
@@ -11,18 +11,17 @@ def createDB():
 	mydb = mysql.connector.connect(
 		host='localhost',
 		user='root',
-		passwd='',
-		database='polovniAutomobili'
+		passwd=''
 	)
 	#database='testDB'
 
 	mycursor = mydb.cursor()
 
 	mycursor.execute("CREATE DATABASE polovniAutomobili")
-	mycursor.execute("SHOW DATABASES")
+	#mycursor.execute("SHOW DATABASES")
 	# to check if database has been created
-	for db in mycursor:
-		print(db)
+	#for db in mycursor:
+	#	print(db)
 
 
 # parse through page using BeautifulSoup
@@ -42,7 +41,10 @@ def get_models(brand):
 	models= soup.find(id='model')
 	models_list=[]
 	for model in models:
-		models_list.append(model.get_text())
+		if model.get_text()=='Ostalo':
+			pass
+		else:
+			models_list.append(model.get_text())
 	return models_list[1:]
 
 
@@ -58,6 +60,16 @@ def all_Brands():
 	return brands_list[1:]
 
 
+def replace(string):
+	string = string.replace('š','s')
+	string = string.replace('Š','s')
+	string = string.replace('ć','c')
+	string = string.replace('Ć','c')
+	string = string.replace('Ž','z')
+	string = string.replace('ž','z')
+	string = string.replace('č','c')
+	string = string.replace('Č','c')
+	return string
 # get all cars from one brand and model
 def get_cars(brand, model):
 
@@ -70,84 +82,122 @@ def get_cars(brand, model):
 	# loop over number of pages
 	for i in range(1,2):
 		#print('page='+str(i))
-		print("")
+		#print("")
 		soup = parse_page(url+'&page='+str(i))
-		pages = soup.findAll('article')
-		for page in pages:
-				#print(page)
-			pageT = page.find('a')
-			#print(pageT)
-			try:
-				title = pageT.get('title')
-				if title==None:
-					pass
-				else:
-					#print(title)
-					discount = page.find(class_='price price-discount')
-					#print(discount.get_text())
-					if discount!=None:
-						#print(discount.get_text())
-						continue
-					else:
-						price = page.find(class_='price')
-						price = price.get_text()
-					content = page.findAll(class_='inline-block')
-					blocks = []
-					for con in content:
-						blocks.append(con.get_text())
-					god = int(blocks[0][:4])
-					km = blocks[1].replace('.','')
-					km = km.replace(' km |', '')
-					km = int(km)
-					gor = blocks[2][:-2]
-					kub = int(blocks[3][:4])
-					kar = blocks[4].replace(',','')
-					sn = blocks[5].replace(', ','')
-					dictionary = {
-							'Brend': (brand.replace('-', ' ')).upper(),
-							'Model': (model.replace('-', ' ')).upper(),
-							'Naziv': title,
-							'Cena': int(price[:-2].replace('.','')),
-							'Godiste': god,
-							'Kilometraza':km,
-							'Gorivo':gor,
-							'Kubikaza':kub,
-							'Karoserija':kar,
-							'Snaga': sn
-					}
-					carList.append(dictionary)
+		#print(soup)
+		if soup!=None:
 
-			except:
-				pass
+			pages = soup.findAll('article')
+			for page in pages:
+					#print(page)
+				pageT = page.find('a')
+				try:
+					title = pageT.get('title')
+					if title==None:
+						pass
+					else:
+						#print(title)
+						discount = page.find(class_='price price-discount')
+						if discount!=None:
+							price = discount.get_text()
+							continue
+						else:
+							price = page.find(class_='price')
+							price = price.get_text()
+						content = page.findAll(class_='inline-block')
+						blocks = []
+						for con in content:
+							blocks.append(con.get_text())
+						god = int(blocks[0][:4])
+						km = blocks[1].replace('.','')
+						km = km.replace(' km |', '')
+						km = int(km)
+						gor = str(blocks[2][:-2])
+						gor = replace(gor)
+						kub = int(blocks[3][:4])
+						kar = (blocks[4].replace(',',''))
+						kar = replace(kar)
+						sn = (blocks[5].replace(', ',''))
+
+						try:
+							price = int(price[:-2].replace('.',''))
+							dictionary = {
+									'Brend': (brand.replace('-', ' ')).upper(),
+									'Model': (model.replace('-', ' ')).upper(),
+									'Naziv': replace(title),
+									'Cena': price,
+									'Godiste': god,
+									'Kilometraza':km,
+									'Gorivo':gor,
+									'Kubikaza':kub,
+									'Karoserija':kar,
+									'Snaga': sn
+							}
+							carList.append(dictionary)
+
+						except:
+							pass
+
+				except Exception as e: 
+					print(e)
+					pass
 	return carList
 
 
 #def more_detail(brand, model):
 
 # function that inserts cars into database
-def insertDB():
+def insertDBAll():
 	mydb = mysql.connector.connect(
-		host='localhost',
-		user='root',
-		passwd='',
-		database='polovniAutomobili'
+		host="localhost",
+		user="root",
+		passwd="",
+		database="polovniAutomobili"
 	)
 
 	mycursor = mydb.cursor()
 
-	mycursor.execute("CREATE TABLE Cars (brend VARCHAR(255), model VARCHAR(255), naziv VARCHAR(255), price INT(10)")
+	#drop = 'DROP TABLE Cars'
+
+	#mycursor.execute(drop)
+
+	#mycursor.execute("CREATE TABLE Cars (brend VARCHAR(255), model VARCHAR(255), naziv VARCHAR(255), cena INT(10), godiste INT(10), kilometraza INT(10), gorivo VARCHAR(255), kubikaza INT(10), karoserija VARCHAR(255) ,snaga VARCHAR(255))")
+
+	sqlInsert = "INSERT INTO Cars (brend, model, naziv, cena, godiste, kilometraza, gorivo, kubikaza, karoserija, snaga) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+	# insert cars by brand and model
+	brands = all_Brands()
+	try:
+		for brand in brands:
+			models = get_models(brand)
+
+			for model in models:
+				if model==None:
+					pass
+				else:
+
+					cars = get_cars(brand, model)
+
+					for car in cars:
+						if car==None:
+							pass
+						else:
+							values = tuple(car.values())
+							try:
+
+								mycursor.execute(sqlInsert, values)
+								mydb.commit()
+							except:
+								pass
+
+	except Exception as e:
+		#print(values)
+		print(e)
+		pass
 
 
 
-
-print(get_cars('bmw','x1'))
-
-
-#createDB();
-
-#brands = all_Brands()
-"""
-for brand in brands:
-	print(brand)
-	print(get_models(brand))
-"""
+#print(all_Brands())
+insertDBAll();
+#print(get_cars('bmw','x1'))
+print("Data inserted")
